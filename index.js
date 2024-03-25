7aac1d92b072b53b0d5f0c777b674665c23638b2
vincent <vinnymummo@gmail.com>
2024-03-25 15:55:44 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+   window.bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'brian'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'boy'
+}
 // Write your solution in this file!
